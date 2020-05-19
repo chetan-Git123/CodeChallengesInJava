2d8496ace5511ad57a4b3173c411a77f09c84586
chetan-Git123 <37156929+chetan-Git123@users.noreply.github.com>
2020-05-19 16:34:27 -0400

    Create ScatteredPalidrome.java

---
+++ ScatteredPalidrome.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList; 
+import java.util.List; 
+
+class ScatteredPalidrome
+{
+	public static void main(String args[])
+	{
+		String inputString = "hajjah";
+		System.out.println("Input Sring is->"+inputString);
+		System.out.println("Is input String palindrome->"+isInputStringPalindrome(inputString));
+	}
+	
+	public static boolean isInputStringPalindrome(String inputString)
+	{
+		//Decalring a List of Characters
+		List<Character> monitorList = new ArrayList<Character>();
+		
+		/* Traverse through all characters of the String. If the character is Not present in the List, include it in the List. 
+		   If the character is in the List, then remove that character. 
+		   To qualify as a scattered palindrome, for a String with odd number of characters, the length of the monitoring List should be 1.
+           To qualify as a scattered palindrome, for a String with even number of characters, length of the monitiring list should be 0.	   
+		*/
+		for(int count=0; count<inputString.length(); count++)
+		{
+			if ( monitorList.contains(inputString.charAt(count)) )
+			{
+				monitorList.remove( (Character)inputString.charAt(count) );
+			}
+			else
+			{
+				monitorList.add( inputString.charAt(count) );
+			}
+		}
+		
+		if(inputString.length()%2==0 && monitorList.isEmpty())
+		{
+			return true;
+		}
+		
+		if(inputString.length()%2==1 && monitorList.size()==1)
+		{
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	
+}
