29eb9706500db138003afd3e262b5c0e0060b4d4
chetan-Git123 <37156929+chetan-Git123@users.noreply.github.com>
2020-05-19 16:36:47 -0400

    Update ScatteredPalidrome.java

--- ScatteredPalidrome.java
+++ ScatteredPalidrome.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList; 
 import java.util.List; 
 
+/*Is Scrambled Palindrome
+Write a function that, given a string of letters, returns true or false for whether the letters in the string could be arranged to form a palindrome.
+E.g. For “torro”, the answer is True, because the letters can be rearranged to spell “rotor”.
+*/
+
 class ScatteredPalidrome
 {
 	public static void main(String args[])
